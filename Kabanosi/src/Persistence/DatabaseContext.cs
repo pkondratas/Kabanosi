de47786ecac7b1f093269f9b274b19ae50ded9c7
Vitas Eliseenko <91189829+suetology@users.noreply.github.com>
2025-04-04 21:29:15 +0300

    Added db configuration (#1)

---
+++ Kabanosi/src/Persistence/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kabanosi.Persistence;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+}
--- Kabanosi/src/Program.cs
+++ Kabanosi/src/Program.cs
@@ -1,4 +1,12 @@
+using Kabanosi.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("AzureMySql");
+
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
