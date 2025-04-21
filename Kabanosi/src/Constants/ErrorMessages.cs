5520e38441b160bb8e350d283eb4fe54953a1e62
whz1gud <vyt.itzip@gmail.com>
2025-04-21 23:03:07 +0300

    Removed expired status and invalidation/deletion of expired invitation logic in other endpoints

--- Kabanosi/src/Constants/ErrorMessages.cs
+++ Kabanosi/src/Constants/ErrorMessages.cs
@@ -3,6 +3,4 @@ namespace Kabanosi.Constants;
 public static class ErrorMessages
 {
     public const string ENTITY_NOT_FOUND = "Entity was not found.";
-    public const string USER_NOT_FOUND = "User was not found.";
-    public const string USER_ALREADY_PROJECT_MEMBER = "User is already a project member.";
 }
