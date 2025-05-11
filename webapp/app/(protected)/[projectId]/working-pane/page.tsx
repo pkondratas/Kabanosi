33333f8ddc04587034193b6c93287fcb9a0ff512
Vitas Eliseenko <91189829+suetology@users.noreply.github.com>
2025-05-11 23:12:14 +0300

    Moved backlog and working-pane components under protected dir (#24)

--- webapp/app/(protected)/[projectId]/working-pane/page.tsx
+++ webapp/app/(protected)/[projectId]/working-pane/page.tsx
@@ -1,10 +1,10 @@
-import Backlog from '@/components/features/backlog/backlog';
+import { WorkingPane } from "@/components/features/protected/working-pane/working-pane";
 
 export default function Page() {
 	return (
 		<div className="flex min-h-screen w-full items-start justify-center p-6 md:p-10">
 			<div className="w-full max-w-screen-lg pt-[70px]">
-				<Backlog />
+				<WorkingPane />
 			</div>
 		</div>
 	);
